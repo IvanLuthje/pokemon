752bc8b84ee2ceb27c2f9717679f48150ad6017c
Ivan Luthje <luthjeivan@gmail.com>
2024-03-29 20:32:35 -0300

    Se agrega funcionalidad JS

--- js/main.js
+++ js/main.js
@@ -10,13 +10,38 @@ function iniciarMap(){
     });
 }
 
-$.ajax({
-    url: 'https://pokeapi.co/api/v2/pokemon/',
-    type: 'GET',
-    dataType: 'json',
-    success: function(Buscar){
-        $('<h1/>').text(json.title).appendTo('busqueda');
-        $('<div class="content"/>')
-        .html(index.html).appendTo('body');
-    }
-})
+$(document).ready(function(){
+    $("#busqueda").click(function(){
+        var nombre_pokemon = $("#nombre").val();
+        $.ajax({
+            url: "https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon,
+            type: "GET",
+            dataType: "json",
+            success: function(dato_pokemon){
+               // $(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1><img src='" + dato_pokemon.sprites.front_default + "' alt='" + dato_pokemon.name + "'><p>Peso: " + dato_pokemon.weight + "</p><p>Altura: " + dato_pokemon.height + "cm</p>");
+               $(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1>" + "<div class='pokemon'>" + "<img src='" + dato_pokemon.sprites.front_default + "'>" + "</div>" + "<p>Peso:" + dato_pokemon.weight + "Kg</p>"  +  "<p>Altura: " + dato_pokemon.height + "m</p>" + "<button class='compartir'>" + "Compartir" + "</button>");
+            },
+            error: function(xhr, status, error) {
+                alert("Pokémon no disponible");
+            }
+        });
+    });
+});
+
+/* $(document).ready(function(){
+    $("#compartir").click(function(){
+        var nombre_pokemon = $("#nombre").val();
+        $.ajax({
+            //url: "https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon.toLowerCase(),
+            url: "https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon,
+            type: "GET",
+            dataType: "json",
+            success: function(dato_pokemon){
+            $(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1>" + "<div class='pokemon'>" + "<img src='" + dato_pokemon.sprites.front_default + "'>" + "</div>" + "<p>Peso:" + dato_pokemon.weight + "Kg</p>"  + "<p>Formas:" + dato_pokemon.forms + "Kg</p>" + "<p>Altura: " + dato_pokemon.height + "m</p>");
+            },
+            error: function(xhr, status, error) {
+                alert("Pokémon no disponible");
+            }
+        });
+    });
+}); */
