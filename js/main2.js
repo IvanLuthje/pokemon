bb703567a9f8f15232d2a04f1e17c27b511ee4ad
IvanLuthje <luthjeivan@gmail.com>
2024-04-12 10:07:17 -0300

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -1,3 +1,15 @@
+function iniciarMap(){
+    var coord = {lat:-34.922883333333, lng:-57.956316666667};
+    var map = new google.maps.Map(document.getElementById('googleMap'),{
+        zoom: 10,
+        center: coord
+    });
+    var marker = new google.maps.Marker({
+        position: coord,
+        map: map
+    });
+}
+
 function navegador() {
     var x = document.getElementById("navegador1");
     if (x.className === "navegador") {
@@ -43,7 +55,7 @@ $(document).ready(function(){
             success: function(dato_pokemon){
 
                // $(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1><img src='" + dato_pokemon.sprites.front_default + "' alt='" + dato_pokemon.name + "'><p>Peso: " + dato_pokemon.weight + "</p><p>Altura: " + dato_pokemon.height + "cm</p>");
-               $(".datos_pokemon").html("<h1>" + dato_pokemon.names[5].name + "</h1>" + "<div class='item'>" + "<img src='" + dato_pokemon.sprites.default + "'>" +  "</div>" + "<p>" + "Costo:  " +  dato_pokemon.cost +  "</p>" + "<p>" + "Tipo:  " +  dato_pokemon.category.name +  "</p>" + "Descripción: " + dato_pokemon.flavor_text_entries[13].text + "</p>" + "<button class='compartir'>" + "Compartir" + "</button>");
+               $(".datos_pokemon").html("<div class ='titulo'>" + "<h1>" + dato_pokemon.names[5].name + "</h1>" + "</div>" + "<div class='item'>" + "<img src='" + dato_pokemon.sprites.default + "'>" +  "</div>" + "<p>" + "Costo:  " +  dato_pokemon.cost +  "</p>" + "<p>" + "Tipo:  " +  dato_pokemon.category.name +  "</p>" + "Descripción: " + dato_pokemon.flavor_text_entries[13].text + "</p>" + "<button class='compartir'>" + "Compartir" + "</button>");
             },
             error: function(xhr, status, error) {
                 alert("Item no encontrado");
@@ -70,6 +82,19 @@ $(document).ready(function(){
         window.location.href='../compartir.html';
     });
 // Modo responsivo
+
+function iniciarMapRes(){
+    var coord = {lat:-34.922883333333, lng:-57.956316666667};
+    var map = new google.maps.Map(document.getElementById('googleMap2'),{
+        zoom: 10,
+        center: coord
+    });
+    var marker = new google.maps.Marker({
+        position: coord,
+        map: map
+    });
+}
+
 
     $("#busqueda_res").click(function(){
         let filtro = document.querySelector('#filtro2');
@@ -120,7 +145,7 @@ $(document).ready(function(){
             success: function(dato_pokemon){
 
             // $(".datos_pokemon").html("<h1>" + dato_pokemon.name + "</h1><img src='" + dato_pokemon.sprites.front_default + "' alt='" + dato_pokemon.name + "'><p>Peso: " + dato_pokemon.weight + "</p><p>Altura: " + dato_pokemon.height + "cm</p>");
-            $(".datos_pokemon").html("<h1>" + dato_pokemon.names[5].name + "</h1>" + "<div class='item'>" + "<img src='" + dato_pokemon.sprites.default + "'>" +  "</div>" + "<p>" + "Costo:  " +  dato_pokemon.cost +  "</p>" + "<p>" + "Tipo:  " +  dato_pokemon.category.name +  "</p>" + "Descripción: " + dato_pokemon.flavor_text_entries[13].text + "</p>" + "<button class='compartir'>" + "Compartir" + "</button>");
+            $(".datos_pokemon").html("<div class ='titulo'>" + "<h1>" + dato_pokemon.names[5].name + "</h1>" + "<div class='item'>" + "<img src='" + dato_pokemon.sprites.default + "'>" +  "</div>" + "</div>" + "<p>" + "Costo:  " +  dato_pokemon.cost +  "</p>" + "<p>" + "Tipo:  " +  dato_pokemon.category.name +  "</p>" + "Descripción: " + dato_pokemon.flavor_text_entries[13].text + "</p>" + "<button class='compartir'>" + "Compartir" + "</button>");
             },
             error: function(xhr, status, error) {
                 alert("Item no encontrado");
@@ -144,7 +169,7 @@ $(document).ready(function(){
     }
     });
     $(".compartir").click(function(){
-        window.location.href='../compartir.html';
+        window.location='compartir.html';
     });
 
 });
